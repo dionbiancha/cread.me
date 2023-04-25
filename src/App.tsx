c26ec5ceda269d1bbd74644350586d03b398e077
Dionei Bianchati <dion.biancha@gmail.com>
2023-04-25 18:00:42 -0300

    feat: darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { Button, useColorMode } from "@chakra-ui/react";
 
 function App() {
   useEffect(() => {
     serviceWorkerRegistration.register();
   }, []);
+  const { colorMode, toggleColorMode } = useColorMode();
 
-  return <div>Hello world</div>;
+  return (
+    <header>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+    </header>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
+import theme from "./theme";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <App />
+  </ChakraProvider>
+);
